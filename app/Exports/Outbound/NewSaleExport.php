13817dc61e49fd43fc0957b1babc126d870a1f69
kodeanas <kodeanas01@gmail.com>
2026-06-04 07:32:07 +0700

    Tambah pelaporan export reguler dan b2b sale

---
+++ app/Exports/Outbound/NewSaleExport.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Exports\Outbound;
+
+use App\Models\Sale;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class NewSaleExport implements FromQuery, WithHeadings, WithMapping
+{
+    protected $startDate;
+    protected $endDate;
+
+    public function __construct($request)
+    {
+        $this->startDate = $request->start_date;
+        $this->endDate = $request->end_date;
+    }
+
+    public function query()
+    {
+        $query = Sale::query();
+
+        if ($this->startDate) {
+            $query->whereDate('created_at', '>=', $this->startDate);
+        }
+
+        if ($this->endDate) {
+            $query->whereDate('created_at', '<=', $this->endDate);
+        }
+
+        return $query;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Document Penjualan',
+            'Barcode Gudang',
+            'Barcode Asal',
+            'Total Item',
+            'Harga Asal',
+            'Harga Jual',
+        ];
+    }
+
+    public function map($sale): array
+    {
+        return [
+            $sale->code_document_sale,
+            $sale->product_barcode_sale,
+            $sale->old_barcode_product,
+            $sale->product_qty_sale,
+            $sale->product_old_price_sale,
+            $sale->product_price_sale,
+        ];
+    }
+}
