d145fa32ee68b168845d408a732b48e2aa68d63b
kodeanas <kodeanas01@gmail.com>
2026-06-11 20:25:17 +0700

    Export cargo terbaru 2

--- app/Http/Controllers/Outbound/ExportOutboundController.php
+++ app/Http/Controllers/Outbound/ExportOutboundController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\ResponseResource;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -15,27 +16,44 @@ class ExportOutboundController extends Controller
 {
     public function exportB2BBaru()
     {
-        $filename = sprintf(
-            'B2B_%s_%s.xlsx',
-            now()->format('dmY'),
-            now()->format('His')
-        );
+        try {
+            $filename = sprintf(
+                'B2B_%s_%s.xlsx',
+                now()->format('dmY'),
+                now()->format('His')
+            );
 
-        $path = 'exports/' . $filename;
+            $path = 'exports/' . $filename;
 
-        Excel::store(
-            new NewCargoExport(),
-            $path,
-            'public'
-        );
+            Excel::store(
+                new NewCargoExport(),
+                $path,
+                'public'
+            );
 
-        return new ResponseResource(
-            true,
-            'Berhasil generate file B2B',
-            [
-                'file_name' => $filename,
-                'link' => Storage::disk('public')->url($path),
-            ]
-        );
+            return new ResponseResource(
+                true,
+                'Berhasil generate file B2B',
+                [
+                    'file_name' => $filename,
+                    'link' => Storage::disk('public')->url($path),
+                ]
+            );
+        } catch (\Throwable $th) {
+
+            Log::error('Export B2B gagal', [
+                'message' => $th->getMessage(),
+                'file' => $th->getFile(),
+                'line' => $th->getLine(),
+            ]);
+
+            return new ResponseResource(
+                false,
+                'Gagal generate file B2B',
+                [
+                    'error' => $th->getMessage(),
+                ]
+            );
+        }
     }
 }
