f338bc6f176e982844af35d09d29c6c7f47459f0
kodeanas <kodeanas01@gmail.com>
2026-06-19 17:18:24 +0700

    Ambil data yang kosong terbaru 2

--- routes/api.php
+++ routes/api.php
@@ -272,13 +272,13 @@ Route::middleware(['auth:sanctum', 'check.role:Admin,Spv,Team leader'])->group(f
 
 Route::middleware(['auth:sanctum', 'check.role:Admin'])->group(function () {
     Route::get('ambil-list-kategori', [AttachCogsController::class, 'getCategories']);
-    Route::get('ambil-list-tidak-ada-kategori-dan-sticker', [AttachCogsController::class, 'getUnmappedSummary']);
+    Route::get('ambil-list-tidak-ada-kategori-dan-sticker', [FixingController::class, 'getUnmappedSummary']);
 });
 
 Route::middleware(['auth:sanctum', 'check.role:Admin,Spv,Team leader'])->group(function () {
     Route::post('attach-cogs-reguler/{document_id}', [AttachCogsController::class, 'attachCogsKeDokumenReguler']);
     Route::post('attach-cogs-sku/{document_id}', [AttachCogsController::class, 'attachCogsKeDokumenSku']);
-    Route::get('channels', [FixingController::class, 'getUnmappedSummary']);
+    Route::get('channels', [AttachCogsController::class, 'getCogsChannels']);
 });
 
 // ========================================================================================================
