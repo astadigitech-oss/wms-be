3fdd5f015667dc32cbb6e0e80950ab44ace4f6f1
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2026-07-03 20:02:05 +0800

    add trap for big query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\PersonalAccessToken;
+use Carbon\CarbonInterval;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
@@ -33,5 +34,17 @@ class AppServiceProvider extends ServiceProvider
         }
 
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+
+        // Perangkap Query Lambat: Catat ke log jika query > 5 detik
+        DB::whenQueryingForLongerThan(CarbonInterval::seconds(5), function ($connection) {
+            Log::warning("Database waduh lemot banget! Total waktu query: {$connection->totalQueryDuration()}ms");
+        });
+
+        // Alternatif: Catat detail query spesifik yang lewat dari 3 detik
+        DB::listen(function ($query) {
+            if ($query->time > 3000) { // waktu dalam milidetik (3000ms = 3 detik)
+                Log::warning("Slow Query Terdeteksi: SQL -> {$query->sql} | Bindings -> " . json_serialize($query->bindings) . " | Waktu -> {$query->time}ms");
+            }
+        });
     }
 }
