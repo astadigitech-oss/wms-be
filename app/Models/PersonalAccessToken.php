4a2484d738a87810d6ae9f31a8506f8e39e6fdcf
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2026-07-04 17:09:32 +0800

    breking change on file PersonalAccesToken

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -15,12 +15,16 @@ class PersonalAccessToken extends SanctumPersonalAccessToken
         // Jika yang berubah HANYA kolom last_used_at (aktivitas request biasa)
         if ($this->isDirty('last_used_at') && ! $this->isDirty(['token', 'abilities', 'name'])) {
 
-            $cacheKey = 'sanctum_token_throttle:' . $this->id;
+            // $cacheKey = 'sanctum_token_throttle:' . $this->id;
 
-            // Cache::add HANYA akan menghasilkan true jika key BELUM ADA di cache (Operasi Atomik)
-            // Jika key SUDAH ADA, dia langsung return false.
-            if (! Cache::add($cacheKey, true, now()->addMinutes(5))) {
-                return true; // Skip update ke MySQL jika gagal menambah cache
+            // // Cache::add HANYA akan menghasilkan true jika key BELUM ADA di cache (Operasi Atomik)
+            // // Jika key SUDAH ADA, dia langsung return false.
+            // if (! Cache::add($cacheKey, true, now()->addMinutes(5))) {
+            //     return true; // Skip update ke MySQL jika gagal menambah cache
+            // }
+            // Jika yang berubah HANYA kolom last_used_at, potong kompas dan return true (jangan kirim query ke MySQL)
+            if ($this->isDirty('last_used_at') && ! $this->isDirty(['token', 'abilities', 'name'])) {
+                return true; 
             }
         }
 
