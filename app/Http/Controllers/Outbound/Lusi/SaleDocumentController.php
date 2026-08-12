7ed48c3fecfbfb5312e91ca9dded4fb3ec604433
kodeanas <kodeanas01@gmail.com>
2026-08-12 13:12:09 +0700

    Benerin pembayaran vouocher forwarder

--- app/Http/Controllers/Outbound/Lusi/SaleDocumentController.php
+++ app/Http/Controllers/Outbound/Lusi/SaleDocumentController.php
@@ -195,7 +195,7 @@ class SaleDocumentController extends BaseSaleDocumentController
             'buyer' => $buyerData,
             'is_voucher_forwarder' => $saleDocument->discount_forwarder != 0,
             'voucher_forwarder' => $saleDocument->discount_forwarder,
-            'value_voucher_forwarder' => (int) ($saleDocument->total_display_document_sale * ($saleDocument->discount_forwarder / 100)),
+            'value_voucher_forwarder' => (int) (($saleDocument->total_display_document_sale - $saleDocument->voucher - $saleDocument->voucher_rank_value) * ($saleDocument->discount_forwarder / 100)),
         ];
 
         return new ResponseResource(true, "data document sale", $resource);
